174cd665cf245ece819a21709a80b0c82b7b53a3
Alexey Panasik <alexey.panasik@itmhouse.com>
2014-09-25 17:36:52 +0300

    fix packer - ability to send array of MessageInterface objects

--- src/SqsWrapper/Packer.php
+++ src/SqsWrapper/Packer.php
@@ -1,6 +1,7 @@
 <?php
 namespace SqsWrapper;
 
+use Guzzle\Http\Message\MessageInterface;
 use Guzzle\Service\Resource\Model;
 
 /**
@@ -90,10 +91,22 @@ class Packer implements PackerInterface {
 					'value' => $this->toJson($value->getMessageData(), get_class($value), false)
 				);
 			} else {
-				$json['properties'][] = array(
-					'name' => $key,
-					'value' => $value
-				);
+				if (is_array($value) && isset($value[0]) && $value[0] instanceof MessageInterface) {
+					$props = array(
+						'name' => $key,
+						'value' => array()
+					);
+					foreach ($value as $k => $obj) {
+						$props['value'][] = $this->toJson($obj->getMessageData(), get_class($obj), false);
+					}
+					$json['properties'][] = $props;
+
+				} else {
+					$json['properties'][] = array(
+						'name' => $key,
+						'value' => $value
+					);
+				}
 			}
 		}
 		return $returnString ? json_encode($json) : $json;
